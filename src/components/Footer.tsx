35c9a1b58068ad48f123405c16f11965187919b3
joe ebene <joyebene153@gmail.com>
2025-01-15 23:24:17 +0100

    fbLink

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="text-green-500">
       <hr className="text-gray-500 mx-4 md:mx-5" />
       <div className="flex items-center justify-evenly py-6 text-base md:text-lg">
-        <span>Facebook</span> <span>Twitter</span> <span> <a href="https://www.instagram.com/adforerunneragro?igsh=a2trMGhybHY5djlq&utm_source=ig_contact_invite">Instagram</a></span>{" "}
+        <span><a href="https://www.facebook.com/share/19ynGv1Aid/?mibextid=wwXIfr">Facebook</a></span> <span>Twitter</span> <span> <a href="https://www.instagram.com/adforerunneragro?igsh=a2trMGhybHY5djlq&utm_source=ig_contact_invite">Instagram</a></span>{" "}
         <span></span>
       </div>
 
